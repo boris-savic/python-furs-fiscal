5788fedfe426463e61da8673452f88acbf248ba7
Boris Savic <boris.savic@xlab.si>
2015-11-07 17:54:14 +0100

    Add test_certificate.pem to the release

--- setup.py
+++ setup.py
@@ -2,14 +2,15 @@ from distutils.core import setup
 setup(
   name = 'furs_fiscal',
   packages = ['furs_fiscal'],
-  version = '0.1.0',
+  version = '0.1.2',
   description = 'Python library for simplified communication with FURS (Financna uprava Republike Slovenije).',
   author = 'Boris Savic',
   author_email = 'boris70@gmail.com',
   url = 'https://github.com/boris-savic/python-furs-fiscal',
-  download_url = 'https://github.com/boris-savic/python-furs-fiscal/tarball/0.1',
+  download_url = 'https://github.com/boris-savic/python-furs-fiscal/tarball/0.1.2',
   keywords = ['FURS', 'fiscal', 'fiscal register', 'davcne blagajne'],
   classifiers = [],
+  data_files=[('certs', ['certs/test_certificate.pem'])],
   install_requires=[
         'requests',
         'python-jose',
