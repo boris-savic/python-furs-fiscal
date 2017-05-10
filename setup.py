213cfa7addb587dc1223070cff7c6d126c764466
Boris Savic <boris.savic@xlab.si>
2017-05-10 13:16:58 +0200

    Updated python-jose

--- setup.py
+++ setup.py
@@ -7,15 +7,15 @@ setup(
   description = 'Python library for simplified communication with FURS (Financna uprava Republike Slovenije).',
   author = 'Boris Savic',
   author_email = 'boris70@gmail.com',
-  url = 'https://github.com/boris-savic/python-furs-fiscal',
-  download_url = 'https://github.com/boris-savic/python-furs-fiscal/tarball/0.4.0',
+  url = 'https://github.com/boris-savic/python_furs_fiscal',
+  download_url = 'https://github.com/boris-savic/python_furs_fiscal/tarball/0.4.0',
   keywords = ['FURS', 'fiscal', 'fiscal register', 'davcne blagajne'],
   classifiers = [],
   package_data={'furs_fiscal': ['certs/*.pem']},
   install_requires=[
         'pytz==2017.2',
         'requests==2.13.0',
-        'python-jose==0.5.4',
+        'python-jose==0.5.6',
         'pyOpenSSL==17.0.0',
     ]
 )
