e4e6a8c5625a9bb24e20366c5c04379a4d419c1f
Boris Savic <boris@olaii.com>
2020-01-17 13:52:43 +0100

    - updated setup.py for 1.0.0 release

--- setup.py
+++ setup.py
@@ -1,21 +1,21 @@
 from setuptools import setup
 
 setup(
-  name = 'furs_fiscal',
-  packages = ['furs_fiscal'],
-  version = '0.6.0',
-  description = 'Python library for simplified communication with FURS (Financna uprava Republike Slovenije).',
-  author = 'Boris Savic',
-  author_email = 'boris70@gmail.com',
-  url = 'https://github.com/boris-savic/python_furs_fiscal',
-  download_url = 'https://github.com/boris-savic/python_furs_fiscal/tarball/0.6.0',
-  keywords = ['FURS', 'fiscal', 'fiscal register', 'davcne blagajne'],
-  classifiers = [],
-  package_data={'furs_fiscal': ['certs/*.pem']},
-  install_requires=[
+    name='furs_fiscal',
+    packages=['furs_fiscal'],
+    version='1.0.0',
+    description='Python library for simplified communication with FURS (Financna uprava Republike Slovenije).',
+    author='Boris Savic',
+    author_email='boris70@gmail.com',
+    url='https://github.com/boris-savic/python_furs_fiscal',
+    download_url='https://github.com/boris-savic/python_furs_fiscal/tarball/1.0.0',
+    keywords=['FURS', 'fiscal', 'fiscal register', 'davcne blagajne'],
+    classifiers=[],
+    package_data={'furs_fiscal': ['certs/*.pem']},
+    install_requires=[
         'pytz>=2017.2',
         'requests>=2.20.0',
-        'python-jose==3.0.1',
+        'python-jose>=3.1.0',
         'pyOpenSSL>=17.5.0',
     ]
 )
